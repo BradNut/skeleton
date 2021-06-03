8a1d523d108f96c7a99c04dfc7aaea890676d89f
Bradley Shellnut <bradleyshellnut@protonmail.com>
2021-06-02 18:18:08 -0700

    Svelete Kit Skeleton up to number 16.

---
+++ src/routes/about.svelte
@@ -0,0 +1,6 @@
+<h1>About Us</h1>
+<p>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti in natus, at laboriosam
+  laudantium amet eum aliquid repellat minima exercitationem officiis quibusdam voluptates eaque
+  assumenda vero consectetur repellendus, illum cum.
+</p>
